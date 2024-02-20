7a5ee7f617e1885efa26a8e965fd507e256e334c
Sérgio Rodrigo <sergio.rodrigo.santos@escola.pr.gov.br>
2024-02-20 13:57:10 -0300

    Exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "olá mundo!";
+const texto2 = 'olá mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Sérgio";
+
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
